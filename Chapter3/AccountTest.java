db2e33babb0788a2eec95cb17f44026d5ce75eee
Kulklex <kulklex@gmail.com>
2022-04-26 15:38:56 -0700

    string.html

--- Chapter3/AccountTest.java
+++ Chapter3/AccountTest.java
@@ -6,13 +6,12 @@ public class AccountTest {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Account myAccount = new Account();
-        System.out.printf("Initial name is : %s%n", myAccount.getName());
         System.out.println("Please Enter Firstname: ");
         String FirstName = input.nextLine();
         System.out.println("Please Enter Surname");
         String SurName = input.nextLine();
         myAccount.setName(FirstName, SurName);
-        System.out.printf("Your Names Are: %s", myAccount.getName());
+        System.out.print("Your Names Are: " + myAccount.getName() );
         input.close();
     }
 }
