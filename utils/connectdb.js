ef427e8d80080a81e3e5ba0924797ae5e810de85
kulklex <kulklex@gmail.com>
2022-09-08 13:14:54 +0100

    settings

--- utils/connectdb.js
+++ utils/connectdb.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 
 async function connectDB(){
-        await mongoose.connect('mongodb+srv://kulklex:Adekunle08118082878@cluster0.4kiujfr.mongodb.net/?retryWrites=true&w=majority', )
+        await mongoose.connect('mongodb+srv://kulklex:<password>@cluster0.4kiujfr.mongodb.net/?retryWrites=true&w=majority', )
         .then(() => console.log("Connected to MongoDB"))
         .catch((err) => console.error(`Failed to connect.... ${err}`))  
 }
